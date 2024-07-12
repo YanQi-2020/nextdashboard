502ff075d4ee84fb847314d900b54327505bc2dd
YanQi-2020 <qiyan0608@gmail.com>
2024-07-12 14:22:28 -0400

    invoices data fetch

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -127,6 +127,7 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+  noStore();
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
